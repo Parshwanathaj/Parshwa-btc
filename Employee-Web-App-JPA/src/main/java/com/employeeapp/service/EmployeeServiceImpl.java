d5139a59303d3377ff2ec950c6c6932fc75e2866
Parshwanatha J <Parshwanatha J@LAPTOP-0QKLNCM0>
2022-07-13 18:55:19 +0530

    Employee Web JPA

---
+++ Employee-Web-App-JPA/src/main/java/com/employeeapp/service/EmployeeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.employeeapp.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.employeeapp.dao.EmployeeDaoJpa;
+import com.employeeapp.dao.IEmployeeDao;
+import com.employeeapp.model.Employee;
+
+public class EmployeeServiceImpl implements IEmployeeService {
+
+	private IEmployeeDao dao;
+
+	public EmployeeServiceImpl() {
+		dao = new EmployeeDaoJpa();
+	}
+
+	@Override
+	public Employee saveEmployee(Employee employee) throws SQLException {
+		return dao.saveEmployee(employee);
+	}
+
+	@Override
+	public Employee findEmployee(int employeeId) throws SQLException {
+
+		return dao.findEmployee(employeeId);
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() throws SQLException {
+		return dao.getAllEmployees();
+	}
+
+	@Override
+	public boolean deleteEmployeeById(int employeeId) throws SQLException {
+		return dao.deleteEmployeeById(employeeId);
+	}
+
+}
