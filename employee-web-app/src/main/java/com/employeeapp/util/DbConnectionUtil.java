d28d892e13a8d5b66e86dd11d2eb15a326371532
Parshwanatha J <Parshwanatha J@LAPTOP-0QKLNCM0>
2022-07-13 12:09:48 +0530

    web app

---
+++ employee-web-app/src/main/java/com/employeeapp/util/DbConnectionUtil.java
@@ -0,0 +1,32 @@
+package com.employeeapp.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnectionUtil {
+
+	static Connection conn = null;
+
+	public static Connection getConnection() {
+		String url = "jdbc:mysql://localhost:3306/parshwa";
+		String user = "root";
+		String password = "root";
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("Connection Successful");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static void closeDbConnection() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
