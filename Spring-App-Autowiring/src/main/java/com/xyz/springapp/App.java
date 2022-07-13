20384276b63894627cb90549ce89ca56ea4bb1df
Parshwanatha J <Parshwanatha J@LAPTOP-0QKLNCM0>
2022-07-13 19:02:09 +0530

    Spring App using Autowiring Pgms

---
+++ Spring-App-Autowiring/src/main/java/com/xyz/springapp/App.java
@@ -0,0 +1,35 @@
+package com.xyz.springapp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//    	Laptop myLaptop = context.getBean(Laptop.class);
+//    	
+//        myLaptop.connectInternet();
+//        myLaptop.joinZoomMeeting();
+//        myLaptop.setRam(16);
+//        
+//        myLaptop.showLaptopInfo();
+//        
+//        Laptop myLaptop2 = context.getBean(Laptop.class);
+//        
+//        myLaptop2.showLaptopInfo();
+//        
+//        Laptop myLaptop3 = context.getBean(Laptop.class);
+//        
+//        myLaptop3.showLaptopInfo();
+
+//        Scanner scan = context.getBean(Scanner.class);
+
+//        System.out.println("Enter a value:");
+//        String value = scan.nextLine();
+//        System.out.println(value);
+//        
+
+	}
+}
---
+++ Spring-App-Autowiring/src/main/java/com/xyz/springapp/InternetConnection.java
@@ -0,0 +1,33 @@
+package com.xyz.springapp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Lazy
+public class InternetConnection {
+
+	@Value("${connection.provider}")
+	private String provider;
+
+	public InternetConnection() {
+		System.out.println("InternetConnetion Object is Created");
+	}
+
+	public String getProvider() {
+		return provider;
+	}
+
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+
+	public void connect() {
+		System.out.println("Connected to Internet | Provider : " + provider);
+	}
+
+	public void joinMeeting() {
+		System.out.println("Joining Meeting..");
+	}
+
+}
