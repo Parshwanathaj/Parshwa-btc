4d991bd2e059f4fd61ea0af064acdae5c3d113c3
Parshwanatha J <Parshwanatha J@LAPTOP-0QKLNCM0>
2022-07-13 18:57:51 +0530

    Exception Handling Example Programs

---
+++ Excepetion Handling/src/com/excepetion/CheckedExceptionTest.java
@@ -0,0 +1,22 @@
+package com.excepetion;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CheckedExceptionTest {
+
+	public static void main(String[] args) {
+		File file = new File("hello.txt");
+		try {
+			try (Scanner scanner = new Scanner(file)) {
+				String str = scanner.nextLine();
+				System.out.println(str);
+			}
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
+
+}
---
+++ Excepetion Handling/src/com/excepetion/InvalidNameException.java
@@ -0,0 +1,14 @@
+package com.excepetion;
+
+public class InvalidNameException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidNameException(String message) {
+		super(message);
+	}
+
+}
